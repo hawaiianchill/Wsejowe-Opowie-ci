3a6ef7f3be7bbe926f1440dfe8a25f3bbf9288ea
Wizzy64 <128071598+Wizzy64@users.noreply.github.com>
2023-04-01 03:32:14 +0200

    ok

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,17 +6,18 @@ using UnityEngine.Animations;
 public class Player : MonoBehaviour
 {
 
+
     public float speed;
     private Vector3 playerPos;
     private Rigidbody2D rb;
-    
+
     void Start()
     {
-        
+
         rb = GetComponent<Rigidbody2D>();
     }
 
-    
+
     void FixedUpdate()
     {
         playerPos.x = Input.GetAxis("Horizontal");
@@ -25,8 +26,41 @@ public class Player : MonoBehaviour
         rb.MovePosition(transform.position + playerPos * speed * Time.deltaTime);
 
         transform.right = new Vector3(playerPos.x, 0, 0);
+    }
 
 
+    public bool gora()
+    {
+        if (playerPos.y == 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
 
+    }
+    public bool dol()
+    {
+        if (playerPos.y == -1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public bool bok()
+    {
+        if ((playerPos.x == 1) || (playerPos.x == -1))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
